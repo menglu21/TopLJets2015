202f1bf2c3948b6d27201f35580f2eaad62e129e
Efe <efe.yazgan@cern.ch>
2020-07-07 10:45:22 +0200

    add charm tagging

--- TopAnalysis/plugins/MiniAnalyzer.cc
+++ TopAnalysis/plugins/MiniAnalyzer.cc
@@ -1084,7 +1084,7 @@ void MiniAnalyzer::recAnalysis(const edm::Event& iEvent, const edm::EventSetup& 
       ev_.j_csv[ev_.nj]     = j->bDiscriminator("pfCombinedInclusiveSecondaryVertexV2BJetTags");
       ev_.j_deepcsv[ev_.nj] = j->bDiscriminator("pfDeepCSVJetTags:probb") + j->bDiscriminator("pfDeepCSVJetTags:probbb");
       ev_.j_btag[ev_.nj]    = (ev_.j_deepcsv[ev_.nj]>0.4941);
-      ev_.j_probc[ev_.nj]   = j->bDiscriminator("pfDeepCSVJetTags:probc");
+      ev_.j_probc[ev_.nj]   = j->bDiscriminator("pfDeepCSVJetTags:probc"); 
       ev_.j_probudsg[ev_.nj]= j->bDiscriminator("pfDeepCSVJetTags:probudsg");
       ev_.j_probb[ev_.nj]   = j->bDiscriminator("pfDeepCSVJetTags:probb");
       ev_.j_probbb[ev_.nj]  = j->bDiscriminator("pfDeepCSVJetTags:probbb");
