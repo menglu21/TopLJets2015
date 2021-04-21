11a68328e315913e3e4eeab45e40ba80c865df99
AlbertHsuNTUphys <58206470+AlbertHsuNTUphys@users.noreply.github.com>
2021-04-22 00:25:51 +0800

    Delete TopAnalysis/test/analysis/ExYukawa/ChargeFlipStudy directory

--- TopAnalysis/test/analysis/ExYukawa/ChargeFlipStudy/run_all_CF.sh
+++
@@ -1,8 +0,0 @@
-plotdir=/eos/user/t/tihsu/DataAnalysis/ChargeFlipStudy/
-python Zmassdistribution.py ${plotdir}
-python SB_seperateModel.py ${plotdir} 1
-python SB_seperateModel.py ${plotdir} 0
-python SB_Detail_Region.py ${plotdir} 1
-python SB_Detail_Region.py ${plotdir} 0
-python SB_Combine_Region.py ${plotdir} 1
-python SB_Combine_Region.py ${plotdir} 0
